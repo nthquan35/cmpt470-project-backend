b4785e9c7a98da71bcc010bd35aa1b28a56983dd
Bolshevik35 <hongquan03051997@gmail.com>
2020-11-01 15:47:30 -0800

    fix script

--- script.js
+++ script.js
@@ -6,9 +6,22 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const db = {
+	users: [
+        {
+	        name: 'testing',
+	        email: 'test@gmail.com',
+	        password: 'pass',
+	        dob: new Date(2000-10-01),
+	        city: 'Vancouver',
+	        joined: new Date()
+    	}	
+	]
+}
+
 
 app.get('/', (req, res) => {
-	res.send('Backend is working');
+	res.json(db.users);
 })
 
 // app.post('/signin', (req, res) => {
@@ -18,10 +31,18 @@ app.get('/', (req, res) => {
 app.post('/register', (req, res) => {
 	const {name, email, password, dob, city} = req.body;
 	// const hash = bcrypt.hashSync(password);
-	res.send(req.body);
+	db.users.push({
+		name: name,
+		email: email,
+		password: password,
+		dob: dob,
+		city: city,
+		joined: new Date()
+	})
+	res.json(db.users[db.users.length -1]);
 })
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
